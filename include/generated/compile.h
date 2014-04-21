5150803c1aeb9424aa30300c63803add59ee9b38
moihack <moihack.inside@gmail.com>
2014-04-21 12:38:09 +0300

    cleaned up with make mrproper - building within CM source tree now works

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 3 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 PREEMPT Mon Apr 14 14:44:18 EEST 2014"
-#define LINUX_COMPILE_BY "moihack"
-#define LINUX_COMPILE_HOST "zanarkand"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.0.101-rc1-perf-g0185682-dirty"
